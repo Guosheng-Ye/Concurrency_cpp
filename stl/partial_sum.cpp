09253960d48e20d8fd5d533cde210e729136a42f
Oneko <2446916542@qq.com>
2024-08-21 09:35:10 +0800

    update comments

--- stl/partial_sum.cpp
+++ stl/partial_sum.cpp
@@ -37,33 +37,35 @@ private:
 template <typename Iterator>
 void paraller_partial_sum(Iterator first, Iterator last) {
     using valueType = typename Iterator::value_type;
-    // typedef typename Iterator::value_type valueType;
-    struct processChunk { // 1
-        /* data */
+
+    struct processChunk {
+
         void operator()(Iterator begin, Iterator last, std::future<valueType> *previousEndValuesFuture_,
                         std::promise<valueType> *endValuesPro_) {
             try {
 
                 Iterator end = last;
                 ++end;
-                std::partial_sum(begin, end, begin); // 2
+                std::partial_sum(begin, end, begin); // 计算当前块的部分和
 
-                if (previousEndValuesFuture_) {                         // 3
-                    valueType addEnd = previousEndValuesFuture_->get(); // 4
-                    *last += addEnd;                                    // 5
-                    if (endValuesPro_) {
-                        endValuesPro_->set_value(*last); // 6
+                if (previousEndValuesFuture_) {                         // 当前块不是第一个块
+                    valueType addEnd = previousEndValuesFuture_->get(); // 获取前一个块的和
+                    *last += addEnd; // 当前块的最后一个元素添加前一个块的和addEnd（**先添加，及时给下一块使用**）
+                    if (endValuesPro_) {                 // 需要将当前块的值传给下一个块
+                        endValuesPro_->set_value(*last); // 将endValuesPro_设置为当前块的最后一个元素的值
                     }
-                    std::for_each(begin, last, [addEnd](valueType &item) { item += addEnd; }); // 7
-                } else if (endValuesPro_) {                                                    //
-                    endValuesPro_->set_value(*last);                                           // 8
+                    std::for_each(begin, last, [addEnd](valueType &item) {
+                        item += addEnd; // 当前块的所有元素（除了最后一个）加上前一个块的和addEnd
+                    });
+                } else if (endValuesPro_) { // 无前一块，直接将当前块的最后一个元素设为endValuesPro_的值
+                    endValuesPro_->set_value(*last); // 提醒 future
                 }
 
-            } catch (...) { // 9
+            } catch (...) {
                 if (endValuesPro_) {
                     endValuesPro_->set_exception(std::current_exception());
                 } else {
-                    throw; // 11
+                    throw;
                 }
             }
         }
@@ -86,10 +88,10 @@ void paraller_partial_sum(Iterator first, Iterator last) {
     t2: (4 9 15).add(6)=>(10,15,21); f2=p1=6, p2=21
     t:  (7 15).add(21) =>(28,36)   ; f3=p2=21
     */
-    std::vector<std::thread>             threads(numThreads - 1);      // 13
-    std::vector<std::promise<valueType>> endValuesPro(numThreads - 1); // 14 前一个线程的累加结果
-    std::vector<std::future<valueType>>  previousEndValuesFuture;      // 15 future获取前一个线程的promise
-    previousEndValuesFuture.reserve(numThreads - 1);                   // 16
+    std::vector<std::thread>             threads(numThreads - 1);
+    std::vector<std::promise<valueType>> endValuesPro(numThreads - 1); // 前一个线程的累加结果
+    std::vector<std::future<valueType>>  previousEndValuesFuture;      // future获取前一个线程的promise
+    previousEndValuesFuture.reserve(numThreads - 1);
     Join_Thread joinThread(threads);
     Iterator    blockStart = first;
 
@@ -97,11 +99,13 @@ void paraller_partial_sum(Iterator first, Iterator last) {
         Iterator blockEnd = blockStart;
         std::advance(blockEnd, blockSize - 1); // 17
 
-        // 18
+        // 不是第一个块线程，传入前一个块的部分和结果，并存储当前块的部分和结果。
         threads[index] = std::thread(processChunk(), blockStart, blockEnd,
                                      (index != 0) ? &previousEndValuesFuture[index - 1] : 0, &endValuesPro[index]);
         blockStart     = blockEnd;
         ++blockStart;
+
+        // 当前块的部分和结果的future对象存入previousEndValuesFuture中，以便传递给下一个线程。
         previousEndValuesFuture.push_back(endValuesPro[index].get_future());
     }
 
